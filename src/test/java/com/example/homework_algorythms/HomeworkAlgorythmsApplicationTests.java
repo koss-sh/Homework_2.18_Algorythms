4d6d0458e4dddcd7058e70ec12f41408fa08aca1
koss-sh <shilinskii22@gmail.com>
2023-05-31 21:19:33 +0300

    initial commit

---
+++ src/test/java/com/example/homework_algorythms/HomeworkAlgorythmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_algorythms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkAlgorythmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
